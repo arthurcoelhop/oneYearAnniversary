d182f06758409836db09c75be62f2774111f0666
Arthur Coelho <157404725+arthurcoelhop@users.noreply.github.com>
2025-03-25 14:22:42 -0300

    Update script.js
    Updating message date.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Mensagem que será escrita
   const fullMessage = "Obrigado por tudo até aqui!";
   const fullText =
-    "Compartilhamos momentos incríveis nesse ano e sou muito grato por isso. Amo você, e estou ansioso para os próximos momentos juntos. Com amor, Arthur. <3 22/03/2025";
+    "Compartilhamos momentos incríveis nesse ano e sou muito grato por isso. Amo você, e estou ansioso para os próximos momentos juntos. Com amor, Arthur. <3 22/03/2024";
 
   // Função para "digitar" a mensagem
   function typeMessage(element, text, delay) {
